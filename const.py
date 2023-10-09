7634af92e307d63d189da34ecd104c537751fb47
z3mois <96260225+z3mois@users.noreply.github.com>
2023-10-09 10:56:22 +0300

    first task

---
+++ const.py
@@ -0,0 +1,3 @@
+DAMP_OF_WIKIDATA_PATh = 'E:\\data_diplom\\wikidata_ru_en_20230703.tar\\wikidata_extracted'
+RUWORDNET_PATH= 'D:\\lbase_data\\ruwordnet.db'
+GOLD_DATA = "D:\\lbase_data\\wiki_merge_predict_test_29_11_22.csv"
